e351c299c860514d22229bf85b03f5a99a66ea73
eNedo <32650507+eNedo@users.noreply.github.com>
2019-06-02 19:03:30 +0200

    Update FileClass.java

--- FileClass.java
+++ FileClass.java
@@ -1,4 +1,12 @@
-
-public class FileClass {
+ public class FileClass {
+private String NameOfFile;
+private byte FileAllocate; 
+private byte isDirectory; 
+private String DateCreated;
+private String DateUpdated;
+private String DateUsed; 
+private int filesize;   
+// dodati bit mapu i funkcije 
+ 
 
 }
