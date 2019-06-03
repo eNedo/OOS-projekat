9c51c7664c66890739411ffea06f510a366cf077
eNedo <32650507+eNedo@users.noreply.github.com>
2019-06-03 22:18:17 +0200

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,28 @@
-public class MainClass
-{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
-    public static void main(String[] args)
-    {
-		System.out.println("OOS projekat!");
-
-    }
+public class MainClass {
+	private static long FILESIZE=1024*1024*20;
+	public static void main(String args[])
+	{
+	try {
+		RandomAccessFile x=new RandomAccessFile("./FILESYSTEM","rw");
+		x.setLength(FILESIZE);		
+		
+		
+		
+		
+		
+		x.close();
+		}
+	catch(FileNotFoundException e)
+	{} 
+	catch(IOException f)
+	{}
+	
+	
+		
+	}
 }
