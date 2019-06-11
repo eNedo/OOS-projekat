d182f2f288c3c372a9fdd49fc6171cbded00878a
NT <NT@NEDO>
2019-06-11 16:36:00 +0200

    z

---
+++ projekat/src/MFT.java
@@ -0,0 +1,15 @@
+
+
+public class MFT
+{
+    private final static int BLOCK_SIZE = 36864;
+    private byte MBA[] = new byte[BLOCK_SIZE];
+
+
+    boolean isFree(int block)
+    {
+        if (MBA[block] == 1) return true;
+        else return false;
+    }
+
+}
---
+++ projekat/src/Utilities.java
@@ -0,0 +1,11 @@
+public class Utilities
+{
+    // za staticke funkcije
+
+    public static String[] splitStringWithSeparator(String inputString, String separator)
+    {
+        return inputString.split(separator);
+
+    }
+
+}
