95b796b730681cf935d6827e8ca0505e844e2af1
eNedo <32650507+eNedo@users.noreply.github.com>
2019-06-13 23:02:06 +0200

    Update DirectoryClass.java

--- src/DirectoryClass.java
+++ src/DirectoryClass.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DirectoryClass
 {
@@ -7,19 +9,52 @@ public class DirectoryClass
     private String DateCreated;
     private String DateUsed;
     private String DateModified;
-    private byte isDirectory;
+    private byte isDirectory=1;
     private byte FileAllocationFlag;
     private byte DepthFlag;
     private byte ResFlag;
     private int Size;
-    // block?
-    private byte signature[] = new byte[64];
-    // funkcije..
- //
-
-    public void flagAfterDirectory(RandomAccessFile randomAccessFile) throws IOException
-    {
-        randomAccessFile.writeChars("FFFFFFFF");
-
+    private int signature ;
+    public DirectoryClass(String name, byte Depth, int sig, int size)
+    { 
+    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'u' HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        DateCreated = formatter.format(date);
+    	Size=size;
+    	NameOfDirectory=name; 
+    	DepthFlag=Depth; 
+    	signature=sig; 
+    	
+    }
+    public void writedir(RandomAccessFile x, FileHeader fh) 
+    { 
+    	try { 
+    	x.seek(0); 
+    	x.seek(102); 
+    	if (fh.NumOfDirs(x)!=1)    
+    	{
+    		for (int i=fh.NumOfDirs(x); i>0; i--) 
+    	x.seek(x.getFilePointer()+168); 
+    	}
+  		     	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'u' HH:mm:ss");
+  		        Date date = new Date(System.currentTimeMillis());
+ 		        DateCreated = formatter.format(date);
+ 		        DateUsed=formatter.format(date); 
+ 		        DateModified=formatter.format(date); 
+  		       for(; NameOfDirectory.length()<15; )
+ 		       NameOfDirectory=NameOfDirectory+" ";
+  		       x.writeChars(NameOfDirectory);
+  		       x.writeChars(DateUsed);
+  		       System.out.println("xxxx");
+  		       x.writeChars(DateCreated);
+  		       x.writeChars(DateModified); 
+  		       x.write(isDirectory);
+  		       x.writeInt(signature);
+  		       x.write(DepthFlag);
+  		       x.write(FileAllocationFlag);
+  		       x.writeInt(Size);
+ 		} 
+    	catch (Exception e) {} 
     }
 }
+
