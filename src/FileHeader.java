92084f8843ef81ca53b3bea331765809b2a7578e
eNedo <32650507+eNedo@users.noreply.github.com>
2019-06-20 12:17:10 +0200

    Update FileHeader.java

--- src/FileHeader.java
+++ src/FileHeader.java
@@ -22,32 +22,85 @@ public class FileHeader
         this.fileSize =filesize;
         this.numberOfBlocks = numberofblocks;
         startBlock=startingblock;
-
-        //TODO: NE KONTAM OVO ISPOD
         this.isMFTfile = isMFTfile;
         if (isMFTfile == 1) DataBlock = datablocks;
     }
 
-    public void writeToFile(RandomAccessFile x, int position)
+    public void writeFiletoMFTheader(RootHeader rh,RandomAccessFile x)
     {
         try
         {
-            x.seek(position);
-            if (isMFTfile == 1) x.write(DataBlock); //64
+        	x.seek(MainClass.ONEMB-106);
+        	 int numberofMFTfiles=0;
+        	    int numberofMFTheaders=0;
+        	    System.out.println(rh.getSizeOfMFTDirs()+" "+rh.getSizeOfMFTFiles());
+        	    byte allocationflag, mftflag;  					//prvo pokretanje
+  if((rh.getSizeOfMFTDirs()+rh.getSizeOfMFTFiles())==0)    x.seek(x.getFilePointer()-64);
+  else {
+        	    for(int i=(rh.getSizeOfMFTDirs()+rh.getSizeOfMFTFiles());i>0;i--)
+        	    { 
+        	    allocationflag=x.readByte(); 
+        	    mftflag=x.readByte(); 
+        	    if(mftflag==1) numberofMFTfiles++; else numberofMFTheaders++; 
+        	    if (isMFTfile==1 && allocationflag==1 && mftflag==1){ x.seek(x.getFilePointer()-66); break;} 
+        	    if (isMFTfile==0 && allocationflag==1) {x.seek(x.getFilePointer()-2); break; }
+        	   if (i==rh.getSizeOfMFTDirs()+rh.getSizeOfMFTFiles()) {
+        	    if (170>((rh.getNumberOfDirectoriums()*255+347)+(numberofMFTfiles*170)+(numberofMFTheaders*106)) )
+					{x.seek(x.getFilePointer()-66); break;}  
+        	    else 		// provjera za granicne slucajeve tj. da li ima memorije za upis
+				if(106>((rh.getNumberOfDirectoriums()*255+347)+(numberofMFTfiles*170)+(numberofMFTheaders*106)) )			
+					 {x.seek(x.getFilePointer()-2); break; }
+        	    x.seek(x.getFilePointer()-106);	//pomjeranje na naredni heder
+        	   }
+        	  } 
+  }	    
+        	// upis 
+        	if (isMFTfile == 1) x.write(DataBlock); //64
             x.writeByte(isAllocated);       //2
             x.writeByte(isMFTfile);
-            x.writeUTF(NameOfFile);      //20  -18 karaktera ime
+            for (;NameOfFile.length()<18;)
+            	NameOfFile=NameOfFile+" "; 
+            x.writeUTF(NameOfFile);      //20  -18 karaktera ime (UTF) 
+            System.out.println(NameOfFile+ " "+ NameOfFile.length()); 
             x.writeInt(fileSize);               //4
             x.writeInt(startBlock);         //4
             x.writeInt(numberOfBlocks);     //4
             x.writeUTF(DateCreated);     //24
             x.writeUTF(DateLastUsed);    //24
             x.writeUTF(DateLastModified);//24
-        } catch (Exception e)
+            if (isMFTfile==1) rh.setSizeOfMFTFiles(rh.getSizeOfMFTFiles()+1); 
+            else rh.setSizeOfMFTDirs(rh.getSizeOfMFTDirs()+1); 
+            rh.stats();
+         } catch (Exception e)
         {
         }
     }
-
+    public int searchMFTfiles(RootHeader rh,RandomAccessFile x,String filename)
+    {
+        try
+        {
+        	rh.stats(); 
+        	x.seek(MainClass.ONEMB-105);
+         	    byte  mftflag; 
+        	    for(int i=rh.getSizeOfMFTDirs()+rh.getSizeOfMFTFiles();i>0;i--)
+        	    { 
+         	    mftflag=x.readByte(); 
+         	    if (mftflag==1) 
+         	    	{ 
+         	    		x.seek(x.getFilePointer()+2); 
+         	    		String temp=x.readUTF(); 
+         	    		System.out.println(temp+ " "+ temp.length()); 
+         	    		if(filename.compareTo(x.readUTF())==0) return 1;
+         	    	}
+         	    if (mftflag==1) x.seek(x.getFilePointer()-171);
+         	    else x.seek(x.getFilePointer()-107);
+        	    } 
+        	    return 0; 		// 0-fajl nije pronadjen
+        } catch (Exception e)	// 1-fajl pronadjen
+        {
+        }
+        return -1;			//greska 
+    }
     public void getDataFromFile(RandomAccessFile x, byte[] temp) throws IOException
     {
         x.read(temp);
